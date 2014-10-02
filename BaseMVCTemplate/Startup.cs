a84ca62f892de9b035697601f19136947bac8ee7
Joe Gebo <jgebo@twc.com>
2014-10-02 14:27:32 -0400

    BaseMVCTemplate
    Base code for MVC Template

---
+++ BaseMVCTemplate/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BaseMVCTemplate.Startup))]
+namespace BaseMVCTemplate
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
